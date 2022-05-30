fbb96b38accd7c3fcb8f2b88b86b0243bc0e3c60
Chang Zhe Ming <zheming2002@gmail.com>
2022-05-30 09:53:01 +0800

    Setup for APK build

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,25 @@
 // from https://stackoverflow.com/questions/72179070/react-native-bundling-failure-error-message-while-trying-to-resolve-module-i
+// with reference to https://facebook.github.io/metro/docs/configuration/#merging-configurations
 
-module.exports = {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
-    //added this
-    resolver: {
-      sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs'],
-    },
-  };
+// module.exports = {
+//     transformer: {
+//       getTransformOptions: async () => ({
+//         transform: {
+//           experimentalImportSupport: false,
+//           inlineRequires: true,
+//         },
+//       }),
+//     },
+//     //added this
+//     resolver: {
+//       sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs'],
+//     },
+//   };
+
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
