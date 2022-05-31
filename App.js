e8964b9419871b07155599eb53d613dee4f02581
Chang Zhe Ming <zheming2002@gmail.com>
2022-06-01 00:46:18 +0800

    Added basic countdown timer

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
+import TimerScreen from './src/screens/TimerScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <LoginScreen />
+      <TimerScreen />
     </View>
   );
 }
