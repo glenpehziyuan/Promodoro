cf92c22cfc898edb328bdff30443daf0e6d8c8d3
Chang Zhe Ming <zheming2002@gmail.com>
2022-07-23 16:05:44 +0800

    Updated UI for LoadingScreen

--- App.js
+++ App.js
@@ -5,23 +5,7 @@ import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <StatusBar style="auto" />
-    //   <AppNavigator />
-    // </View>
 
     <AppNavigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  timer: {
-    backgroundColor: 'coral',
-  },
-});
